d448982c6c680a6e934183766757410d592f7b79
Hoang Quang An <anhq@intra.nttdata.com.vn>
2023-07-24 15:13:58 +0700

    video5

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 9001
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! voi Hoi dan it')
+})
+
+app.get('/about', (req, res) => {
+  res.send('Toi la Hoang Quang An')
 })
 
 app.listen(port, () => {
